2af6b7be2d5fe47e5f4274458037cf86a115199c
William Hawley <bill@hawleyfamily.com>
2026-05-13 17:38:09 -0400

    Clean up gitignore and remove junk files

--- scripts/test_algolia.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.post("https://a4149apl2c.algolia.net/1/indexes/sermons", headers={"X-Algolia-Application-Id":"A4149APL2C","X-Algolia-API-Key":"3c79ff84634df740423519749e1c94a8","Content-Type":"application/json"}, json={"objectID":"test_001","title":"Test"})
-print(r.status_code, r.text)
